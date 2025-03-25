abdfedf048c6ed819ad0e580da6eb6d894bac6d4
Speakblue <malrieux@outlook.com>
2025-03-25 19:34:57 +0100

    Revert "rajout test"
    This reverts commit f3c9b9958db87d52eeeae9d21f3bfd0a86cf0b03.

--- public/runtime_dispositif.js
+++ public/runtime_dispositif.js
@@ -188,7 +188,7 @@ let remainingPublicSubjectsToChoose = [];
 
 /**
  * Choix d'un sujet au hasard, avec la certitude que tous les sujets vont être choisis une fois
- * puis on remet tous les sujets et on tire une nouvelle fois, et ainsi de suite Test
+ * puis on remet tous les sujets et on tire une nouvelle fois, et ainsi de suite
  */
 function chooseRandomPublicSubject() {
   // Reload list if necessary
